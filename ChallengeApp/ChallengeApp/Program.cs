058f09c50d235635cdc69617392d8b626720df3b
Oliwia Grzegrzółka <oliwia.grzegrzolka.01@gmail.com>
2023-08-15 12:15:18 +0200

    Zadanie dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "Kobieta";
+int age = 30;
+
+if (age < 30 && sex == "kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (age < 18 && sex != "kobieta")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (age > 18 && sex != "kobieta")
+{
+    Console.WriteLine("Pełnoletni mężczyzna")
+}
